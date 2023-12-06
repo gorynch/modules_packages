182b56571cc312191c3d68026f3c9f6a6d23322f
gorynch <i231081@yandex.ru>
2023-12-06 16:11:42 +0300

    Just added a print of image2ascii.VERSION

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from application.solary import calculate_salary
 from application.db.people import get_employees
 from datetime import datetime
 import subprocess
+import image2ascii
+import subprocess
 
 if __name__ == '__main__':
     print("Let's start")
@@ -9,5 +11,7 @@ if __name__ == '__main__':
     print('now_date: ', datetime.now())
     calculate_salary()
     get_employees()
+    print('image2ascii: ',image2ascii.VERSION)
+    subprocess.run(['image2ascii', 'python_logo.png', '--color', '--fill'])
     print()
     print('Done')
